c654793981c01bbb2d94385568de47deca175836
Maksim Menshikov <maksimchik8741@gmail.com>
2022-08-25 09:45:04 +0300

    Modify '.gitignore'. Add ';' to script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 const enteredNubber1=prompt('Введите первое произвольное число');
-const enteredNubber2=prompt('Введите второе произвольное число')
+const enteredNubber2=prompt('Введите второе произвольное число');
 const result=Number(enteredNubber1)+Number(enteredNubber2);
 alert(enteredNubber1+'+'+enteredNubber2+'='+result);
