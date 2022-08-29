f0d3278912cc2c4c51b9ccb3efd12dcd57846654
Maksim Menshikov <maksimchik8741@gmail.com>
2022-08-29 13:42:53 +0300

    Homework2

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-const enteredNubber1=prompt('Введите первое произвольное число');
-const enteredNubber2=prompt('Введите второе произвольное число');
-const result=Number(enteredNubber1)+Number(enteredNubber2);
-alert(enteredNubber1+'+'+enteredNubber2+'='+result);
+let operandA;
+let operandB;
+let mathMetod;
+let result;
+
+operandA=inputData(1);
+operandB=inputData(2);
+
+do{
+    mathMetod=prompt("Выберите действие ( + - * / )");
+}while (mathMetod===null||mathMetod===''||(mathMetod!=='-'&&mathMetod!=='+'&&mathMetod!=='*'&&mathMetod!=='/'))
+
+switch(mathMetod){
+    case "+":
+        result=operandA+operandB; alert(`${operandA}+${operandB}=${result}`); 
+    break;
+    case "-":
+        result=operandA-operandB; alert(`${operandA}-${operandB}=${result}`); 
+    break;
+    case "*":
+        result=operandA*operandB; alert(`${operandA}*${operandB}=${result}`); 
+    break;
+    case "/":
+        result=operandA/operandB; alert(`${operandA}/${operandB}=${result}`); 
+    break;
+    
+}
+function inputData(num)
+{
+do{
+    val=prompt(`Введите ${num} число :`);
+}while (val===null||val===''||isNaN(val))
+val=Number(val);
+return val;
+}
