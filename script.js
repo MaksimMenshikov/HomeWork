5f0e61e8509dbf3293d36f01f511b68a42b0ea5d
Maksim Menshikov <maksimchik8741@gmail.com>
2022-09-08 11:48:22 +0300

    HomeWork 5

---
+++ script.js
@@ -0,0 +1,55 @@
+const action = getAction();
+const operands = getOperands('Введите операнды через запятую');
+let result=null;
+const operandsArray=operands.split(",");
+
+result=+operandsArray[0];
+for(let i=1;i<operandsArray.length;i++){
+     result = calculate(result,+operandsArray[i]);
+}
+alert(`Результат : ${result}`);
+
+
+//***********Описание функций**************/
+function getAction() {
+    let action = prompt('Выбирете действие  + - * / ');
+
+    while (isActionInvalid(action)) {
+        action = prompt('Некорректный ввод.Выбирете действие  + - * / ');
+    }
+    return action;
+}
+
+function isActionInvalid(action) {
+    return action !== '+' && action !== '-' && action !== '*' && action !== '/';
+}
+
+
+function getOperands(title) {
+    let operand = prompt(title);
+
+    while (isOperandsInvalid(operand)) {
+        operand = prompt(title);
+    }
+    return operand;
+}
+
+function isOperandsInvalid(operand) {
+     return operand === null || operand.trim() === '';
+}
+
+function calculate(a, b) {
+    switch (action) {
+      case '+':
+        return a + b;
+      case '-':
+        return a - b;
+      case '*':
+        return a * b;
+      case '/':
+        return a / b;
+    }
+  }
+
+
+
