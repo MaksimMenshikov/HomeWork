8348c0aa2b941daa9e1edea82990c190b9e7c6fa
Maksim Menshikov <maksimchik8741@gmail.com>
2022-09-12 12:24:08 +0300

    homeWork 6

--- script.js
+++ script.js
@@ -1,53 +1,31 @@
-const action = getAction();
-const operandsArray = getOperands('Введите операнды через запятую');
-let result=+operandsArray[0];
-for(let i=1;i<operandsArray.length;i++){
-     result = calculate(result,+operandsArray[i]);
-}
-alert(`Результат : ${result}`);
 
-
-//***********Описание функций**************/
-function getAction() {
-    let action = prompt('Выбирете действие  + - * / ');
-
-    while (isActionInvalid(action)) {
-        action = prompt('Некорректный ввод.Выбирете действие  + - * / ');
-    }
-    return action;
-}
-
-function isActionInvalid(action) {
-    return action !== '+' && action !== '-' && action !== '*' && action !== '/';
-}
-
-
-function getOperands(title) {
-    let operand = prompt(title);
-
-    while (isOperandsInvalid(operand)) {
-        operand = prompt(title);
-    }
-   
-    return operand.split(",");
-}
-
-function isOperandsInvalid(operand) {
-     return operand === null || operand.trim() === '';
-}
-
-function calculate(a, b) {
-    switch (action) {
-      case '+':
-        return a + b;
-      case '-':
-        return a - b;
-      case '*':
-        return a * b;
-      case '/':
-        return a / b;
-    }
+const students = [
+  {   id:10,
+      name: 'John Smith',
+      marks: [10, 8, 6, 9, 8, 7 ]
+  },
+  {   id:11,
+      name: 'John Doe',
+      marks: [ 9, 8, 7, 6, 7 ]
+  },
+  {   id:12,
+      name: 'Thomas Anderson',
+      marks: [6, 7, 10, 8 ]
+  },
+  {   id:13,
+      name: 'Jean-Baptiste Emanuel Zorg',
+      marks: [10, 9, 8, 9 ]
   }
+]
+calculateStudentAverageMark(students[2]);
+calculateGroupAverageMark(students);
 
+function calculateStudentAverageMark(student){
+  return student.marks.reduce((acc,item)=>acc+item)/student.marks.length;
+  
+}
 
-
+function calculateGroupAverageMark(studentsArrays){
+  const allMarksArray=studentsArrays.reduce((acc,item)=>acc.concat(item.marks),[]);
+  return allMarksArray.reduce((acc,item)=>acc+item)/allMarksArray.length;
+}
