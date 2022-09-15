ffec64e68822f0ad6eb10f50bf36d40f3ab14ec8
Maksim Menshikov <maksimchik8741@gmail.com>
2022-09-15 11:11:57 +0300

    homeWork 9

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
+function createCalculator(value) {
+  let counts = value;
 
-const students = [
-  {   id:10,
-      name: 'John Smith',
-      marks: [10, 8, 6, 9, 8, 7 ]
-  },
-  {   id:11,
-      name: 'John Doe',
-      marks: [ 9, 8, 7, 6, 7 ]
-  },
-  {   id:12,
-      name: 'Thomas Anderson',
-      marks: [6, 7, 10, 8 ]
-  },
-  {   id:13,
-      name: 'Jean-Baptiste Emanuel Zorg',
-      marks: [10, 9, 8, 9 ]
-  }
-]
-calculateStudentAverageMark(students[2]);
-calculateGroupAverageMark(students);
-
-function calculateStudentAverageMark(student){
-  return everageArray(student.marks);
-}
-  
-function calculateGroupAverageMark(studentsArrays){
-  const allMarksArray=studentsArrays.reduce((acc,item)=>acc.concat(item.marks),[]);
-  return everageArray(allMarksArray);
-}
-
-function everageArray(value){
- return value.reduce((acc,item)=>acc+item)/value.length;
+  return {
+      add: (val) => counts+=val,
+      sub: (val) => counts-=val,
+      div: (val) => counts=counts/val,
+      mult: (val) => counts=counts*val,
+      set: (val) => (counts = val),
+  };
 }
