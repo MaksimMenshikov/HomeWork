1e5992927724ad75f8b9da887e907432052a0984
Maksim Menshikov <maksimchik8741@gmail.com>
2022-09-15 11:18:15 +0300

    homeWork 7

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function createCalculator(value) {
   let counts = value;
 
   return {
-      add: (val) => counts+=val,
+      sum: (val) => counts+=val,
       sub: (val) => counts-=val,
       div: (val) => counts=counts/val,
       mult: (val) => counts=counts*val,
