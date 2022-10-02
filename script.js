59eee1c6d6efa7a1572aa546daea89c989420f75
Maksim Menshikov <maksimchik8741@gmail.com>
2022-10-02 12:17:33 +0300

    HomeWork 11 Todolist с данными

--- script.js
+++ script.js
@@ -1,84 +1,143 @@
+const TASK_NOT_DONE_CLASS = 'task-not-done';
 const DELETE_BTN_CLASS = 'delete-btn';
+const TASK_DONE_CLASS = 'task-done';
 
-const contactsListEl = document.querySelector('#contactsList');
-const nameEl = document.querySelector('#name');
-const surnameEl = document.querySelector('#surname');
-const phoneEl = document.querySelector('#phone');
-const addBtnEl = document.querySelector('#addContactBtn');
-const errorMessageEl = document.querySelector('#errorContainer');
-const formInput=document.querySelector('#ContactListInput');
 
+const idEl = document.querySelector('#id');
+const taskListEl = document.querySelector('#taskList');
+const taskNameInput = document.querySelector('#taskNameInput');
+const newTaskForm = document.querySelector('#newTaskForm');
+const errorContainerEl = document.querySelector('#errorContainer');
 const taskITemTemplate = document.querySelector('#todoItemTemplate').innerHTML;
 
-addBtnEl.addEventListener('click', onAddContactBtnClick);
-contactsListEl.addEventListener('click', onFormClick);
-formInput.addEventListener('input', onFormInput);
+newTaskForm.addEventListener('submit', onNewTaskFormSubmit);
+taskNameInput.addEventListener('input', onTaskNameInput);
+taskListEl.addEventListener('click', onListClick);
 
-function onAddContactBtnClick(){
-   if (!validateInput()) return;
-    const newContact = getFormValues();
 
-    addContact(newContact);
+
+let list = [
+    { id: 1, title:'Task 1',status:TASK_NOT_DONE_CLASS},
+    { id: 2, title:'Task 2',status: TASK_NOT_DONE_CLASS },
+    { id: 3, title:'Task 3',status:TASK_DONE_CLASS},
+];
+
+init();
+
+function onNewTaskFormSubmit(event) {
+    event.preventDefault();
+
+    if (!validateInput()) return;
+
+    const newTodo = getFormData();
+    saveTodo(newTodo);
     resetForm();
-    
 }
 
-function onFormInput(){
+
+function onListClick(e) {
+    if (e.target.classList.contains(DELETE_BTN_CLASS)) {
+        const TodoId = getTodoId(e.target.parentElement);
+        deleteTodo(TodoId);
+    }
+    else if (e.target.classList.contains(TASK_NOT_DONE_CLASS)) {
+        toggleTodo(e.target);
+        const TodoId = getTodoId(e.target);
+        changeStatus(TodoId,"done");
+           }
+    else if (e.target.classList.contains(TASK_DONE_CLASS)) {
+        toggleTodo(e.target);
+        const TodoId = getTodoId(e.target);
+        changeStatus(TodoId,"notDone")
+    }
+    }
+
+function init() {
+    renderTodoList(list);
+}
+function renderTodoList(list) {
+    taskListEl.innerHTML = '';
+    list.forEach(renderTodo);
+}
+function renderTodo(Todo) {
+    const todoHtml = getTodoHtml(Todo);
+
+    taskListEl.insertAdjacentHTML('beforeend', todoHtml);
+}
+
+function getTodoHtml({ id, title,status }) {
+    
+    return taskITemTemplate
+        .replaceAll('{{id}}', id)
+        .replaceAll('{{title}}', title)
+        .replaceAll('{{status}}', status);
+}
+
+function saveTodo(Todo) {
+    if (!Todo.id) {
+        addTodo(Todo);
+}
+}
+
+function addTodo(Todo) {
+    Todo.id = Date.now();
+    list.push(Todo);
+    renderTodoList(list);
+}
+
+
+
+function deleteTodo(id) {
+    list = list.filter((item) => item.id !== id);
+
+    renderTodoList(list);
+}
+
+function getTodoId(elem) {
+    return +elem.dataset.contactId;
+}
+
+
+function onTaskNameInput() {
     validateInput();
 }
 
-function onFormClick(event) {
-    
-    if (event.target.classList.contains(DELETE_BTN_CLASS)) {
-        deleteContact(event.target.parentElement.parentElement);
-    }
-}
-function deleteContact(contactEl) {
-    contactEl.remove();
-}
-
-
-function getFormValues(){
-    return {
-        name: nameEl.value,
-        surname: surnameEl.value,
-        phone: phoneEl.value,
-    }
-}
-
-function addContact(contact) {
-   
-    const contactHtml = generateContactHtml(contact);
-    contactsListEl.insertAdjacentHTML('beforeEnd', contactHtml); 
-}
-
-function generateContactHtml(contactAdded) {
-    
-   let template= taskITemTemplate.replaceAll('{{name}}', contactAdded.name ) 
-      .replaceAll('{{surname}}', contactAdded.surname)
-      .replaceAll('{{phone}}', contactAdded.phone);
-    return template;
-}
-
-function resetForm(){
-    nameEl.value = '';
-    surnameEl.value = '';
-    phoneEl.value = '';
-}
-
 function validateInput() {
-    const value =getFormValues();
-    return validateValues(value);
+    const value = taskNameInput.value;
+
+    return validateValue(value);
 }
 
-function validateValues(value) {
-    if ((value.name === '')||(value.surname=== '')||(value.phone=== '')) {
-        errorMessageEl.textContent ='Adding a new contact is impossible, entered all fields';
-        addBtnEl.disabled = true;
+function getFormData() {
+    return {
+        title: taskNameInput.value,
+        status:TASK_NOT_DONE_CLASS
+    };
+}
+
+function resetForm() {
+    taskNameInput.value = '';
+}
+
+function toggleTodo(todoEl) {
+    todoEl.classList.toggle(TASK_DONE_CLASS);
+}
+
+function validateValue(value) {
+    if (value === '') {
+        errorContainerEl.textContent = 'Todo Name is required';
+        submitBtn.disabled = true;
         return false;
     } else {
-        errorMessageEl.textContent = '';
-        addBtnEl.disabled = false;
+        errorContainerEl.textContent = '';
+        submitBtn.disabled = false;
+
         return true;
     }
+}
+function changeStatus(id,action){
+    const selectedTodo = list.find((item) => item.id === id);
+    if (action==="done") selectedTodo.status=TASK_DONE_CLASS;
+    else if (action==="notDone")selectedTodo.status=TASK_NOT_DONE_CLASS;
+    renderTodoList(list);
 }
