cca5614bc1501ebe5d21b7fca9cf565f8cfb363f
Maksim Menshikov <maksimchik8741@gmail.com>
2022-11-05 10:04:59 +0200

    added static variable

--- view/FormView.js
+++ view/FormView.js
@@ -2,10 +2,20 @@ class FormView {
     el=null;
     #config = null;
     taskNameInput = document.querySelector('#taskNameInput');
-    constructor(config){
-        this.init();
-        this.#config = config;
-    }
+    static formTemplate=`<form id="newTaskForm">
+    <div class="row">
+        <div class="ten columns">
+            <input
+                type="text"
+                id="taskNameInput"
+                class="u-full-width"
+            />  
+        </div>
+        <div class="two columns">
+            <input type="submit" id="submitBtn" value="Add" />
+        </div>
+    </div>
+</form>`;
     static getFormValues() {
         return {
             title: taskNameInput.value,
@@ -14,23 +24,14 @@ class FormView {
     static resetForm() {
         taskNameInput.value = '';
     }
+    constructor(config){
+        this.init();
+        this.#config = config;
+    }
     
     init(){
         this.el = document.createElement('div');
-        this.el.innerHTML=`<form id="newTaskForm">
-                    <div class="row">
-                        <div class="ten columns">
-                            <input
-                                type="text"
-                                id="taskNameInput"
-                                class="u-full-width"
-                            />  
-                        </div>
-                        <div class="two columns">
-                            <input type="submit" id="submitBtn" value="Add" />
-                        </div>
-                    </div>
-                </form>`;
+        this.el.innerHTML=FormView.formTemplate;
         this.el.addEventListener('submit', (e) => {
             e.preventDefault();
             const newTask = FormView.getFormValues();
