bb248a52db93ce06b51ad0cc069dd0d556f84254
Maksim Menshikov <maksimchik8741@gmail.com>
2022-11-13 10:51:01 +0200

    HomeWork 19 Image Gallery

--- common/js/utils.js
+++
@@ -1,7 +0,0 @@
-function htmlToElement(html) {
-    const container = document.createElement('div');
-
-    container.innerHTML = html;
-
-    return container.children[0];
-}
--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-new StickerController($('.container'));
+const fotoTemplate = document.querySelector('#fotoTemplate').innerHTML;
+const $imgEl = $('#fotorama')
+let list=[]; 
+bildImageGallery();
+  
+  function bildImageGallery() {
+   return fetch('https://jsonplaceholder.typicode.com/photos?_limit=10') 
+        .then((res) => res.json())
+        .then((data) => {
+            list = data;
+            renderImageList(list);
+            createGallery();
+        });
+        
+    }
+function renderImageList(list) {  
+    $imgEl.html = '';
+    list.forEach(renderImage);
+}
+function renderImage(image) {
+    const currentImage = getImage(image);
+    $imgEl.append('beforeend',currentImage);
+}
+
+function getImage({url, title}) {
+    return fotoTemplate
+        .replaceAll('{{url}}', url)
+        .replaceAll('{{title}}', title);
+}
+function createGallery(){
+    $imgEl.fotorama();
+}
